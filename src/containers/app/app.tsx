70a05acf21ef40b1e16b9eff85634201ec91ce55
Bukovski <kmanankov@bk.ru>
2020-01-16 00:03:35 +0300

    add: react router for navigation

--- src/containers/app/app.tsx
+++ src/containers/app/app.tsx
@@ -1,63 +1,93 @@
 import React from 'react';
+import { NavLink, Redirect, Route, Switch } from 'react-router-dom'
 import Tooltip from '../tooltip';
 import TableEdit from '../table-edit';
-import { TABLE_FIELDS, TABLE_EMPLOYERS } from '../../config';
-import "./app.css"
+import { TABLE_EMPLOYERS, TABLE_FIELDS } from '../../config';
 import Workers from "../workers";
 import ColorForm from "../color-form";
+import "./app.css"
 
 
-export default function App(props: object) {
+export default function App() {
   return(
     <React.Fragment>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Color Form</a>
+          <li className="nav-item">
+            <NavLink
+              to="/"
+              className="nav-link"
+              exact
+              activeClassName={'active'}
+            >
+              Color Form
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Tooltip</a>
+            <NavLink
+              to="/tooltip"
+              className="nav-link"
+              activeClassName={'active'}
+            >
+              Tooltip
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Edit Table</a>
+            <NavLink
+              to="/edit-table"
+              className="nav-link"
+              activeClassName={'active'}
+            >
+              Edit Table
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Workers Table</a>
+            <NavLink
+              to="/workers"
+              className="nav-link"
+              activeClassName={'active'}
+            >
+              Workers Table
+            </NavLink>
           </li>
         </ul>
       </nav>
 
-      <ColorForm />
 
-      <hr/>
+      <Switch>
+        <Route path="/" exact component={ ColorForm }/>
 
-      <div className="row">
-        <Tooltip
-          text="Master Express.js-The Node.js Framework For Your Web Development"
-          allowToggleWithClick={false}
-          allowToggleWithMouseInteraction={true}
-          positionWhereShowText="top"
-        >
-          Pro Express.js
-        </Tooltip>
-        was published in 2014. It was one of the first books on v4.x. And it was my second book published with Apress after
-        <Tooltip text="Practical Node.js: Building Real-World Scalable Web Apps">Practical Node.js</Tooltip>.
-        ...
-        The main focus of this post is to compare the four Node.js/Io.js frameworks:
-        <Tooltip text="HTTP API server">Hapi</Tooltip>,
-        <Tooltip text="Release the Kraken!">Kraken</Tooltip>,
-        <Tooltip text="Sail away">Sails.js</Tooltip> and
-        <Tooltip text="IBM of frameworks">Loopback</Tooltip>.
-        There are many other frameworks to consider, but I had to draw the line somewhere.
-      </div>
+        <Route path="/tooltip" render={ () => <div className="row">
+          <Tooltip
+            text="Master Express.js-The Node.js Framework For Your Web Development"
+            allowToggleWithClick={false}
+            allowToggleWithMouseInteraction={true}
+            positionWhereShowText="top"
+          >
+            Pro Express.js
+          </Tooltip>
+          was published in 2014. It was one of the first books on v4.x. And it was my second book published with Apress after
+          <Tooltip text="Practical Node.js: Building Real-World Scalable Web Apps">Practical Node.js</Tooltip>.
+          ...
+          The main focus of this post is to compare the four Node.js/Io.js frameworks:
+          <Tooltip text="HTTP API server">Hapi</Tooltip>,
+          <Tooltip text="Release the Kraken!">Kraken</Tooltip>,
+          <Tooltip text="Sail away">Sails.js</Tooltip> and
+          <Tooltip text="IBM of frameworks">Loopback</Tooltip>.
+          There are many other frameworks to consider, but I had to draw the line somewhere.
+        </div>
+        } />
 
-      <hr/>
+        <Route path="/edit-table" render={ () => <TableEdit
+          employers={ TABLE_EMPLOYERS }
+          fields={ TABLE_FIELDS }
+        />
+        } />
 
-      <TableEdit employers={ TABLE_EMPLOYERS } fields={ TABLE_FIELDS }/>
+        <Route path="/workers" component={ Workers }/>
 
-      <hr/>
-
-      <Workers/>
+        <Redirect to={'/'}/>
+      </Switch>
     </React.Fragment>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './containers/app';
 import 'bootstrap/dist/css/bootstrap.min.css'; //from node_modules
 import './styles/main.css';
 
 
-render(<App />, document.getElementById('root') as HTMLElement);
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root') as HTMLElement
+);
